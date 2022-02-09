cad9b7e7826edc36243990239534e045f1696dd9
Deepthi Das <98408725+ddas6313@users.noreply.github.com>
2022-02-09 12:56:16 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,8 +7,7 @@ terraform {
     }
   }
 
-  
- backend "remote" {
+  backend "remote" {
     organization = "SREYO"
 
     workspaces {
@@ -79,11 +78,14 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "3.2.0"
 
-  name                 = "${var.cluster_name}-vpc"
-  cidr                 = "10.0.0.0/16"
-  azs                  = data.aws_availability_zones.available.names
-  private_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  name = "${var.cluster_name}-vpc"
+  cidr = "10.0.0.0/16"
+  //azs                  = data.aws_availability_zones.available.names[0]
+  azs = ["us-east-2a", "us-east-2b"]
+  //private_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  //public_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  private_subnets      = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets       = ["10.0.4.0/24", "10.0.5.0/24"]
   enable_nat_gateway   = true
   single_nat_gateway   = true
   enable_dns_hostnames = true
